86b6312556d4390ee86dee3038bba43a0f673286
maxnyquist <max.nyquist@mass.gov>
2022-01-03 11:04:54 -0500

    Cleanup and remove reference to .rds and db.

--- biobase_csv_process.R
+++ biobase_csv_process.R
@@ -16,9 +16,9 @@ sapply(pkg, library, character.only = TRUE)
   options(scipen = 999)
 
 ### SOURCE DATA/FUNCTIONS/FILES ####
-
-R_Config <- read.csv("//env.govt.state.ma.us/enterprise/DCR-WestBoylston-WKGRP/WatershedJAH/EQStaff/WQDatabase/R-Shared/Code/R_Config.csv", header = TRUE)
-config <- as.character(R_Config$CONFIG_VALUE)
+# 
+# R_Config <- read.csv("//env.govt.state.ma.us/enterprise/DCR-WestBoylston-WKGRP/WatershedJAH/EQStaff/WQDatabase/R-Shared/Code/R_Config.csv", header = TRUE)
+# config <- as.character(R_Config$CONFIG_VALUE)
 
 biobase_dir <- "W:/WatershedJAH/EQStaff/Aquatic Biology/GIS/ciBiobase_sl2"
 ### Annual Data 
@@ -30,8 +30,8 @@ biobase_processed <- paste0(biobase_year, "/processed csv")
 ### Run after downloading from biobasemaps.com 
 csv_files <- list.files(biobase_raw,full.names = TRUE, pattern = "\\.csv$")
 csv_files
-### Update with file to be processed
-csv_in <- c(5:10)
+### Manually update with file to be processed
+csv_in <- c(11)
 csv_files <- csv_files[csv_in]
 data <- lapply(csv_files, read_csv)
 df_name <- gsub(".csv", "", list.files(biobase_raw, pattern = "\\.csv$"))
@@ -44,45 +44,6 @@ rm(data)
 rm('NA')
 
 ### Need to put in a loop/funciton to go through each one of these not in the "processed" folder  
-
-
-# ### FETCH CACHED DATA FROM WAVE RDS FILES ####
-#   datadir <- config[1]
-#   ### Make a list of all the .rds files using full path
-#   rds_files <- list.files(datadir,full.names = TRUE ,pattern = "\\.rds$")
-#   rds_files # Take a look at list of files
-#   ### Select which rds files needed for this script
-#   rds_in <- c(3,4,7:9)
-#   ### subset rds files (if you want all of them then skip rds_in and the following line)
-#   rds_files <- rds_files[rds_in]
-#   ### create an object that contains all of the rds files
-#   data <- lapply(rds_files, readRDS)
-#   ### Make a list of the df names by eliminating extension from files
-#   df_names <- gsub(".rds", "", list.files(datadir, pattern = "\\.rds$"))
-#   df_names <- df_names[rds_in]
-#   # name each df in the data object appropriately
-#   names(data) <- df_names
-#   ### Extract each element of the data object into the global environment
-#   list2env(data ,.GlobalEnv)
-#   ### Remove data
-#   rm(data)
-
-### CONNECT TO A FRONT-END DATABASE ####
-  
-  ### Set DB
-  #db <- config[3]
-  ### Connect to Database 
-  #con <- dbConnect(odbc::odbc(),
-   #                .connection_string = paste("driver={Microsoft Access Driver (*.mdb)}",
-    #                                          paste0("DBQ=", db), "Uid=Admin;Pwd=;", sep = ";"),
-     #              timezone = "America/New_York")
-  ### See the tables 
-  #tables <- dbListTables(con)  
-  ### Fetch an entire table (avoid using SQL queries - just pull entire tables and subset in R)
-  #tbl <- dbReadTable(con, "tblParameters")
-  ### Always disconnect and rm connection when done with db
-  #dbDisconnect(con)
-  #rm(con)
 
 ### SCRIPT BEGIN ####
 for (i in data)
